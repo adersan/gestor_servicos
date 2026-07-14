8a3493d6fe5a0fcff019b4f1ef8caea9d7436b64
Aderval Santiago <adersan@hotmail.com>
2026-07-13 22:00:20 -0300

    Adiciona botao flutuante + para lancar novo servico em todas as paginas
    Botao fixo no canto inferior direito (desktop e mobile/tablet), acima do toast e da barra de
    navegacao inferior, reaproveitando o data-dialog="serviceDialog" ja tratado pelo listener
    delegado generico. Some quando o menu mobile esta aberto. Cache bump: styles.css v87, sw.js
    register v107, sw.js cache v126.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gestor-servicos-v125";
+const CACHE = "gestor-servicos-v126";
 const ASSETS = [
   "./",
   "index.html",
@@ -6,7 +6,7 @@ const ASSETS = [
   "logo.svg",
   "icon-192.png",
   "icon-512.png",
-  "styles.css?v=86",
+  "styles.css?v=87",
   "cliente.css?v=24",
   "config.js?v=30",
   "auth.js?v=30",
